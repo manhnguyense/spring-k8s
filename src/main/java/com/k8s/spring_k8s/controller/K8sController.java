0b0dd69d00e8081cf2042986198b1e368e6c18a5
Manh Nguyen <manhnv5@vng.com.vn>
2025-05-30 10:08:46 +0700

    multi env

--- src/main/java/com/k8s/spring_k8s/controller/K8sController.java
+++ src/main/java/com/k8s/spring_k8s/controller/K8sController.java
@@ -1,13 +1,13 @@
 package com.k8s.spring_k8s.controller;
 
 import com.k8s.spring_k8s.config.AppConfig;
+import com.k8s.spring_k8s.config.entity.K8s;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,9 +19,10 @@ public class K8sController {
 
     @GetMapping("/namespace")
     public ResponseEntity<String> getK8sNamespace() {
-      final String namespace =  appConfig.getK8s().getNamespace();
-      log.info("Fetching Kubernetes namespace: {}", namespace);
+        final K8s k8s = appConfig.getK8s();
+        final String namespace = k8s.getNamespace();
+        log.info("Fetching Kubernetes namespace: {}, env: {}", namespace, appConfig);
       return ResponseEntity.status(HttpStatus.OK)
-          .body(namespace);
+          .body(appConfig.toString());
     }
 }
